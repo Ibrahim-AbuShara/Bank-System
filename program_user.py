02411d87f92e3e3c234b086bd5ef43acffbb76c9
A7med-Ro7oma <Ra7ouma70@gmail.com>
2021-10-03 17:56:47 +0200

    Adding Role Parameter & Try except

--- program_user.py
+++ program_user.py
@@ -1,10 +1,10 @@
 class ProgramUser:
 
     def __init__(self):
-        self.AccData = []          # list for passing (user_name & password) from/to  files
+        self.AccData = []          # list for passing (user_name & password & role) from/to  files
         self.logged = False        # login default condition
 
-    def register(self, username, password):
+    def register(self, username, password, role):
         conditions = True
         # if len(str(username)) < 10:
         #     print("please enter your full name ")
@@ -14,20 +14,24 @@ class ProgramUser:
         #     conditions = False
         if conditions:                                # user name & password are accepted
             print("Account created successfully")
-            self.AccData = [username, password]       # now the list have username at loc 0 & pass at loc 1
-            with open(f"{username}.txt", "w") as f:   # create new file for writing user & pass (file name = username)
+            self.AccData = [username, password, role]  # now the list have username at loc 0 & pass at loc 1 & role at 2
+            with open(f"{username}.txt", "w") as f:    # CreateNewFileForWriting user&pass&role (file name = username)
                 for details in self.AccData:
-                    f.write(str(details) + "\n")      # passing name & pas to the new file
+                    f.write(str(details) + "\n")       # passing name & pas & role to the new file
         else:
-            print("Process Failed")                 # user name & password are not accepted
+            print("Process Failed")                    # user name & password are not accepted
 
     def login(self, username, password):
-        with open(f"{username}.txt", "r") as f:  # search for file with this name (need exception if file doesn't exist)
-            details = f.read()                   # reading name & pass
-            self.AccData = details.split("\n")   # using split to get the list back
-            if password == self.AccData[1]:      # remember password at loc 1
-                self.logged = True               # if the password is correct then the flag now true
-            if self.logged:
-                print(f"{username} logged in")
-            else:
-                print("Wrong details")
+        try:
+            with open(f"{username}.txt", "r") as f:  # search for file with this name (exception if file doesn't exist)
+                details = f.read()                   # reading name & pass & role
+        except:
+            print("File Doesn't Exist .. ")
+            exit()
+        self.AccData = details.split("\n")   # using split to get the list back
+        if password == self.AccData[1]:      # remember password at loc 1
+            self.logged = True               # if the password is correct then the flag now true
+        if self.logged:
+            print(f"{username} logged in as {self.AccData[2]}") # remember role at loc 2
+        else:
+            print("Wrong details")
