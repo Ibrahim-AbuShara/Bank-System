8b08cc7e851ce49cbbb1724152c8cbab93492f6f
OmarElsool <omarbaka1999@gmail.com>
2021-10-04 22:44:42 +0200

    Add Customer Class

--- customers.py
+++ customers.py
@@ -1,8 +1,46 @@
-class Customer:
-    #inhirt from Progrma user
-    #ovride login method
-    #ovride the __init__ --> balnce pramitare
-    #def cash withdraw--> taxes 15% 
-    #deposit money
-    #def display palnce
-    pass
+from program_user import ProgramUser
+
+
+# This func to Read The data from file and change the line you want
+def modify_line(file_open, line_tochange, new_value):
+    with open(file_open, 'r') as file:
+        data = file.readlines()  # read a list of lines into data
+    data[line_tochange] = str(new_value) + "\n"  # line_tochange is the index num of line to change and new_value is
+    # the new value to write
+    with open(file_open, 'w') as file:  # open the file to write into
+        file.writelines(data)  # write the new data after change in the file
+
+
+class Customer(ProgramUser):
+
+    def __init__(self):
+        super().__init__()  # inherit from Program user
+        self.balance = 0  # put balance 0 on new acc
+        self.AccData = []  # list to access username in the class [0] = username, [1] = password, [2] = role,
+        # [3] = balance
+
+    # overrider Reg Method to add balance
+    def register(self, username, password, role):
+        super().register(username, password, role)  # inherit the register func from ProgramUser
+        self.AccData = [username, password, role, self.balance]  # put data in AccData to access it later
+        with open(f"{username}.txt", "a") as f:  # append the balance variable when the user register
+            f.write(f"{self.balance}\n")
+
+    # def cash withdraw--> taxes 15%
+    def withdraw_amount(self, amount):  # withdraw func
+        if self.balance >= amount:
+            self.balance -= (15 / 100) * amount + amount
+            modify_line(f"{self.AccData[0]}.txt", 3, self.balance)  # use modify_line function
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Your Balance Is Less Than Withdraw ")
+
+    # deposit money
+    def deposit_amount(self, amount):  # deposit func
+        self.balance += amount
+        modify_line(f"{self.AccData[0]}.txt", 3, self.balance)  # use modify_line function
+        print("\n Amount Deposited: ", amount)
+
+    # def display balance
+    def show_balance(self):  # function to display the balance
+        print(f"Welcome {self.AccData[0]} Your Balance is : ", self.balance)
--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from accountants import Accountant
 from admins import Admin
-from program_user import Prgram_User
+from program_user import ProgramUser
 from customers import Customer
 from super_customer import Super_Customer
 
-enter=int(input('chose from 1-4 \n 1-admin \n 2-accountat \n 3-super customer \n 4-customer \n > '))
+enter = int(input('chose from 1-4 \n 1-admin \n 2-accountat \n 3-super customer \n 4-customer \n > '))
 if enter == 1:
-    #obj=Admin
-    #obj
-    #obj
-   pass
+    # obj=Admin
+    # obj
+    # obj
+    pass
 elif enter == 2:
-    #obj=Accountant
-    #obj
-    #obj
+    # obj=Accountant
+    # obj
+    # obj
     pass
 elif enter in 3:
-    #obj=Super_Customer
-    #obj
-    #obj
-   pass
-elif enter==4:
-    #obj=Customer
-    #obj
-    #obj
+    # obj=Super_Customer
+    # obj
+    # obj
     pass
-else :
-    print(['not valid'])   
+if enter == 4:
+    customer = Customer()
+    username = input('Enter Your username')
+    password = int(input('Enter Your Password'))
+    customer.register(username, password, 'customer')
+else:
+    print(['not valid'])
